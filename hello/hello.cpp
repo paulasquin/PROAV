8280033f99a9558cd4d7f8eb8cd1b0fc050dd33d
paulasquin <paul.asquin@gmail.com>
2017-10-25 08:59:26 +0200

    premier commit

---
+++ hello/hello.cpp
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello PROAV, allez allez les bleus et blancs de l'aviron bayonais" << endl;
+	return 0;
+}
