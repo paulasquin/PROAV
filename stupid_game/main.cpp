2b46904d995e6bdcbfb1c494bed8dc35c454a3f2
paulasquin <paul.asquin@gmail.com>
2017-10-25 09:13:59 +0200

    ajout du projet stupid_game

---
+++ stupid_game/main.cpp
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int nombre
+	cout << "Hello PROAV, allez allez les bleus et blancs de l'aviron bayonais" << endl;
+	return 0;
+}
